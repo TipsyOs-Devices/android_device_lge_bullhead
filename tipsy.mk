43038e0ca04de72549185b16e9ae70eda419cd7d
JosipKC <d3hum4niz3d@gmail.com>
2017-01-22 12:01:41 +0100

    add tipsy.mk
    Add enchanced nfc, some caf stuff and tipsyfy

--- tipsy.mk
+++ tipsy.mk
@@ -24,14 +24,18 @@ PRODUCT_RELEASE_NAME := bullhead
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+$(call inherit-product, vendor/tipsy/config/caf_fw.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/tipsy/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 PRODUCT_DEVICE := bullhead
-PRODUCT_NAME := slim_bullhead
+PRODUCT_NAME := tipsy_bullhead
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 5X
 PRODUCT_MANUFACTURER := LGE
